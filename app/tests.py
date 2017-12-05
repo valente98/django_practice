3e5cabf047751ac2a1c27b2f9f3c57e4945a04c7
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-05 09:50:37 -0600

    created function to test the add view and to test the double view:

--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,43 @@
 from django.test import TestCase
+from django.urls import reverse
+
+
+class TestAddView(TestCase):
+    def test_without_nums_doesnt_set_answer(self):
+        response = self.client.get(reverse('app:add'))
+
+        answer = response.context.get('answer')
+
+        self.assertIsNone(answer)
+
+    def test_add_two_two_equals_four(self):
+        response = self.client.get(
+            reverse('app:add'),
+            {'num1': 2,
+             'num2': 2}, )
+
+        answer = response.context.get('answer')
+
+        self.assertEqual(answer, 4)
+
+    def test_add_four_negthree_equals_one(self):
+        response = self.client.get(
+            reverse('app:add'),
+            {'num1': 4,
+             'num2': -3}, )
+
+        answer = response.context.get('answer')
+
+        self.assertEqual(answer, 1)
+
+
+class TestDoubleView(TestCase):
+    def test_without_num_doesnt_set_answer(self):
+        response = self.client.get(reverse('app:double'))
+
+        answer = response.context.get('answer')
+
+        self.assertIsNone(answer)
+
 
 # Create your tests here.
