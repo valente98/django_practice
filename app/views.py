380f2ef86b8a14c15b3eadad4bf2c4bd6c18d78b
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-05 10:57:38 -0600

    added more to the if statement then created a function that allows the user to know how much the earning are.

--- app/views.py
+++ app/views.py
@@ -30,7 +30,7 @@ def multThree(request):
     num2 = request.GET.get('num2')
     num3 = request.GET.get('num3')
 
-    if num1 is not None:
+    if num1 is not None and num2 is not None and num3 is not None:
         num1 = float(num1)
         num2 = float(num2)
         num3 = float(num3)
@@ -39,3 +39,19 @@ def multThree(request):
         return render(request, 'app/multThree.html', {'answer': answer})
     else:
         return render(request, 'app/multThree.html')
+
+
+def earning(request):
+    num1 = request.GET.get('num1')
+    num2 = request.GET.get('num2')
+    num3 = request.GET.get('num3')
+
+    if num1 is not None and num2 is not None and num3 is not None:
+        num1 = float(num1)
+        num2 = float(num2)
+        num3 = float(num3)
+
+        answer = num1 * 15 + num2 * 12 + num3 * 9
+        return render(request, 'app/earning.html', {'answer': answer})
+    else:
+        return render(request, 'app/earning.html')
