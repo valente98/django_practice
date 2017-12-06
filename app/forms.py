41aca0d1e4608d8739dba169b803c498e95c9b88
valente alvarez <valvarez@basecampcodingacademy.org>
2017-12-06 08:55:23 -0600

    created forms for the multThree view and for earning view.

--- app/forms.py
+++ app/forms.py
@@ -14,3 +14,22 @@ class DoubleForm(forms.Form):
 
     def answer(self):
         return self.cleaned_data['num'] * 2
+
+
+class MultThreeForm(forms.Form):
+    num1 = forms.FloatField()
+    num2 = forms.FloatField()
+    num3 = forms.FloatField()
+
+    def answer(self):
+        return self.cleaned_data['num1'] * self.cleaned_data[
+            'num2'] * self.cleaned_data['num3']
+
+
+class Earningsform(forms.Form):
+    seat1 = forms.FloatField()
+    seat2 = forms.FloatField()
+    seat3 = forms.FloatField()
+
+    def answer(self):
+        return self.cleaned_data['seat1'] * 15 + self.cleaned_data['seat2'] * 12 + self.cleaned_data['seat3'] * 9
